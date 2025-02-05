9bb7158d744e7826571a877423c1af2b9a1a6ae8
TcanTahiroglu <cantahiroglu73@gmail.com>
2025-02-05 15:37:39 +0300

    Update App.jsx

--- kisiselwebsitesi/src/App.jsx
+++ kisiselwebsitesi/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
+import Header from './components/header'
 import Skills from './components/skills'
 import Profile from './components/profile'
 import Projects from './components/projects'
